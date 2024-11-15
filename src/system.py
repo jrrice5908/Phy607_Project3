2227cd3aee25057081699b8c4329ac16a85cb5a8
mavesben <benmaves@Bens-MacBook-Pro.local>
2024-11-14 20:05:54 -0500

    update

--- src/system.py
+++ src/system.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 #from .particle import Particle
 import particle as pp
 class System:
@@ -44,27 +45,27 @@ class System:
 					part_or_list.append(particle2.orientation)
 			self.mean_orientation_list.append(np.mean(part_or_list))
 				
-	def compute_energy(self, alpha = 1.0, beta = 0.2):
+	def compute_energy(self, alpha = 1, beta = 25):
 		self.total_energy = 0
 		for i in self.particles:
 			for j in self.particles:
 				d = i.find_distance(j)
 				orientation_diff = np.cos(i.orientation - j.orientation)
-				pair_energy = - alpha * orientation_diff - beta / (1 + d **2)
+				pair_energy = - alpha * orientation_diff**2 - beta / (1 + d **2)
 				self.total_energy += pair_energy / 2
 		return self.total_energy
 
-	def compute_new_energy(self, alpha = 1.0, beta = 0.2):
+	def compute_new_energy(self, alpha = 1, beta = 25):
 		self.new_total_energy = 0
 		for i in self.particles:
 			for j in self.particles:
 				d = i.find_new_distance(j)
 				orientation_diff = np.cos(i.new_orientation - j.new_orientation)
-				pair_energy = - alpha * orientation_diff - beta / (1 + d ** 2)
+				pair_energy = - alpha * orientation_diff**2 - beta / (1 + d ** 2)
 				self.new_total_energy += pair_energy / 2
 		return self.new_total_energy
 
-	def mcmc_proposal(self, position_step=0.1, orientation_step=0.1):
+	def mcmc_proposal(self, position_step=0.5, orientation_step=0.5):
 		prior_energy = self.compute_energy()
 		for part in self.particles:
 			if np.random.rand() < 1:
@@ -84,14 +85,37 @@ class System:
 		prior_energy = self.compute_energy()
 		prop_energy = self.compute_new_energy()
 		delta_e = prop_energy - prior_energy
-		if delta_e < 0 or np.random.rand() < np.exp(-delta_energy/ self.temp):
+		if delta_e < 0 or np.random.rand() < np.exp(-delta_e/ self.temp):
+			print("yes")
 			for i in self.particles:
 				i.position = i.new_pos
 				i.orientation = i.new_orientation
-
+		else:
+			print("nah fam")
+	def plot_system(self):
+		plt.figure(figsize= (6,6))
+		plt.xlim(0,self.length)
+		plt.ylim(0, self.length)
+		x = []
+		y = []
+		dx = []
+		dy = []
+		for i in self.particles:
+			x = (i.position[0])
+			y = (i.position[1])
+			dx = (np.cos(i.orientation) * 0.3)
+			dy = (np.sin(i.orientation) * 0.3)
+			plt.arrow(x,y,dx,dy, head_width =  -0.1, head_length = 0.2, fc = 'blue', ec = 'blue')
+		plt.gca().set_aspect('equal', adjustable = 'box')
+		plt.show()
+			
 system = System(10, 100,0.1)
-system.mcmc_step()
-
+system.plot_system()
+for i in range(1000):
+	#system.plot_system()
+	system.mcmc_step()
+	print(system.total_energy)
+system.plot_system()
 #    def add_particle(self, particle):               #System.add_particle, adds particle to class list
 #        #Add a particle to the simulation box
 #        self.particles.append(particle)
