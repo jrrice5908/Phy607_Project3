0c5ada43d24983b88a8ad909b580a03a85cbfdf2
jrricesyr <jrrice@syr.edu>
2024-11-15 11:47:38 -0500

    edit

--- src/system.py
+++ src/system.py
@@ -116,27 +116,6 @@ for i in range(1000):
 	system.mcmc_step()
 	print(system.total_energy)
 system.plot_system()
-#    def add_particle(self, particle):               #System.add_particle, adds particle to class list
-#        #Add a particle to the simulation box
-#        self.particles.append(particle)
-        
-#    def initialize_positions(self, N, length):      #System.initialize_positions, returns [position,[vx,vy]]
-#        particles = []
-#        for i in range(N):
-#            position = np.random.uniform(0, length, (N, 2))
-#            particle = Particle(position, np.zeros(2))    #Particles are initialized with speed=0
-#            particles.append(particle)
-#        return particles
-        
-#    def initialize_speeds(self, mean_speed, speed_variability):     #System.initialize_speeds
-#        #Initialize particle velocities by sampling from Gaussian distribution centered around mean
-#        for particle in self.particles:
-#            particle.speed = np.random.normal(mean_speed, speed_variability, N)
-            
-#    def intitialize_orientations(self, N):              #System.initialize_orientations
-#        for particle in self.particles:
-#            particle.orientation = np.random.uniform(0, 2 * np.pi, N)
-
 
 
 
